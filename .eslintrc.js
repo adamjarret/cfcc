a06a7e4ff1687407bcb1457aa86409ef1d5112b7
Adam Jarret <adam@atj.me>
2020-06-05 21:24:54 -0400

    Updated dependencies. Switched from minimist to argv-walk

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,29 @@
 module.exports = {
-    "env": {
-        "es6": true,
-        "node": true
+    'env': {
+        'commonjs': true,
+        'es2015': true,
+        'node': true
     },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": 2015
+    'extends': 'eslint:recommended',
+    'parserOptions': {
+        'ecmaVersion': 2015
     },
-    "rules": {
-        "indent": [
-            "error",
+    'rules': {
+        'indent': [
+            'error',
             4
         ],
-        "linebreak-style": [
-            "error",
-            "unix"
+        'linebreak-style': [
+            'error',
+            'unix'
         ],
-        "quotes": [
-            "error",
-            "single"
+        'quotes': [
+            'error',
+            'single'
         ],
-        "semi": [
-            "error",
-            "always"
+        'semi': [
+            'error',
+            'always'
         ]
     }
 };
--- src/purgeCacheOptions.js
+++ src/purgeCacheOptions.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-const parseArgs = require('minimist');
+const walkArgv = require('argv-walk');
 
 const maybeLoadFile = (opts, relTo = '') => !opts.config || !opts.config.length ? opts : (
     Object.assign(fromFile(path.resolve(relTo, opts.config)), opts)
@@ -8,14 +8,21 @@ const maybeLoadFile = (opts, relTo = '') => !opts.config || !opts.config.length 
 
 function fromArgv(argv)
 {
-    const options = parseArgs(argv);
-    const {_: files} = options;
+    const options = {};
 
-    delete options._;
-
-    if (files && files.length) {
-        options.files = files;
-    }
+    walkArgv(argv, {
+        boolean: ['version'],
+        onArg: (arg) => {
+            if (arg.key) {
+                options[arg.key] = arg.value;
+            } else {
+                if(!options.files) {
+                    options.files = [];
+                }
+                options.files.push(arg.item);
+            }
+        }
+    });
 
     return maybeLoadFile(options);
 }
